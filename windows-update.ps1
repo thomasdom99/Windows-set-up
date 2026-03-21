1222b57ded07425f154cf2dc1de7b731fa3d8056
Thomas <50990614+thomasdom99@users.noreply.github.com>
2026-03-21 21:23:01 +1000

    Update windows-update.ps1

--- windows-update.ps1
+++ windows-update.ps1
@@ -152,8 +152,8 @@ if (Test-Path $adobePath) {
 }
 
 # Google Drive
-$googledrivePath = "C:\Program Files\Google\Drive File Stream\googledrivesync.exe"
-if (Test-Path $googledrivePath) {
+$googledrivePath = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like "*Google Drive*" }
+if ($googledrivePath) {
     Write-Host "  [OK] Google Drive already installed, skipping." -ForegroundColor Green
 } else {
     Write-Host "  [Downloading] Downloading Google Drive..." -ForegroundColor Yellow
