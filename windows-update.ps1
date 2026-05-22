5602e2b985b8a37668b643a73064d24c06dcfec3
Thomas <50990614+thomasdom99@users.noreply.github.com>
2026-05-22 19:53:32 +1000

    Add 1Password and Sublime Text to update script

--- windows-update.ps1
+++ windows-update.ps1
@@ -44,7 +44,9 @@ $PACKAGES = @(
     "winrar",
     "revo-uninstaller",
     "notepadplusplus",
-    "epicgameslauncher"
+    "epicgameslauncher",
+    "1password",
+    "sublimetext4"
 )
 
 Write-Host "Updating Chocolatey..." -ForegroundColor Cyan
@@ -81,6 +83,8 @@ foreach ($package in $PACKAGES) {
         "revo-uninstaller" { "Revo Uninstaller" }
         "notepadplusplus"  { "Notepad++" }
         "epicgameslauncher"{ "Epic Games Launcher" }
+        "1password"        { "1Password" }
+        "sublimetext4"     { "Sublime Text" }
         default            { $package }
     }
     $registryInstalled = Get-ItemProperty $registryPaths -ErrorAction SilentlyContinue |
